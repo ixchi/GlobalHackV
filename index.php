f9dad77bc54060752578d95c020d4df80b966442
Greg Cordover <ghc@ixchi.me>
2015-09-12 01:21:16 -0500

    updates and stuff

--- index.php
+++ index.php
@@ -20,8 +20,12 @@ $klein->respond('GET', '/', function ($request, $response, $service, $app) {
 });
 
 $klein->respond('POST', '/search', function ($request, $response, $service, $app) {
+	if (!$request->first_name || !$request->last_name || !$request->date_of_birth) {
+		return $response->redirect('/');
+	}
+
 	$db = $app->db;
-	$stmt = $db->prepare('SELECT * FROM `good_data` WHERE `last_name` LIKE :name AND `date_of_birth` = :birthday');
+	$stmt = $db->prepare('SELECT * FROM `good_data_fixed` WHERE `last_name` LIKE :name AND `date_of_birth` = :birthday ORDER BY `status_date` DESC');
 	$name = "%{$request->last_name}%";
 	$stmt->bindParam(':name', $name);
 	$stmt->bindParam(':birthday', $request->date_of_birth);
@@ -30,10 +34,33 @@ $klein->respond('POST', '/search', function ($request, $response, $service, $app
 	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 	if (count($results) === 0) {
-		return $service->twig->render('noresults.twig');
+		return $service->twig->render('noresults.twig', array(
+			'first' => $request->first_name,
+			'last' => $request->last_name,
+			'birthday' => $request->date_of_birth
+		));
 	}
 
-	return $service->twig->render('search.twig', array('results' => $results));
+	$info = new stdClass();
+	foreach ($results as $row) {
+		if ($row['status']  == 'FTA WARRANT ISSUED') {
+			if (!$info->warrant) $info->warrant = array();
+			$info->warrant[] = $row['citation_number'];
+		}
+
+		if ($row['status']  == 'CONT FOR PAYMENT') {
+			if (!$info->fine) $info->fine = array();
+			$info->fine[] = array(
+				'amount' => $row['fine_amount'],
+				'citation' => $row['citation_number']
+			);
+		}
+	}
+
+	return $service->twig->render('search.twig', array(
+		'results' => $results,
+		'info' => $info
+	));
 });
 
 $klein->dispatch();
