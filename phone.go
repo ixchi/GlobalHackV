2b9e97e6926a9f6f489df8352c482f8462bc8056
Greg Cordover <ghc@ixchi.me>
2015-09-12 14:34:23 -0500

    By works, I meant 'doesnt work'. Works now.. (http://whatthecommit.com)

--- phone.go
+++ phone.go
@@ -5,9 +5,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
+var data map[string]map[string]interface{}
+
+func parseMessage(text string) (string, bool) {
+	parts := strings.Split(text, "\n")
+	if len(parts) != 3 {
+		return "Not enough lines! Please include your last name, birth date (YYYY-MM-DD), and driver's license ID.", false
+	}
+
+	return "", true
+}
+
 func main() {
+	data = make(map[string]map[string]interface{})
+
 	nexmoClient, _ := nexmo.NewClientFromAPI(os.Getenv("NEXMO_KEY"), os.Getenv("NEXMO_SECRET"))
 	balance, err := nexmoClient.Account.GetBalance()
 	if err != nil {
@@ -20,7 +34,13 @@ func main() {
 
 	go func() {
 		for msg := range messages {
-			log.Printf("%+v", msg)
+			nexmoClient.SMS.Send(&nexmo.SMSMessage{
+				From:  "13374192686",
+				To:    msg.MSISDN,
+				Type:  nexmo.Text,
+				Text:  "Hello! :)",
+				Class: nexmo.Standard,
+			})
 		}
 	}()
 
