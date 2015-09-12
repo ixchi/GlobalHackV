19348a65fc9d568d4af64c3ad7016ec7cd06df4f
Greg Cordover <ghc@ixchi.me>
2015-09-12 14:24:25 -0500

    totally more readable (http://whatthecommit.com)

--- phone.go
+++ phone.go
@@ -4,16 +4,23 @@ import (
 	"github.com/njern/gonexmo"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	nexmoClient, _ := nexmo.NewClientFromAPI(os.Getenv("NEXMO_KEY"), os.Getenv("NEXMO_SECRET"))
+	balance, err := nexmoClient.Account.GetBalance()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(balance)
+
 	messages := make(chan *nexmo.RecvdMessage)
 	h := nexmo.NewMessageHandler(messages, false)
 
 	go func() {
-		for {
-			msg := <-messages
-			log.Printf("%v\n", msg)
+		for msg := range messages {
+			log.Println(msg)
 		}
 	}()
 
