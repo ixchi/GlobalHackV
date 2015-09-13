448cd6b7eec94d8b4c3aee3baca6b16fce1fbe8e
Greg Cordover <ghc@ixchi.me>
2015-09-13 11:11:51 -0500

    fix answer

--- phone.py
+++ phone.py
@@ -157,7 +157,7 @@ def results(request):
     cur = db.cursor()
 
     cur.execute('SELECT court_date, court_location, violation_description, first_name, last_name, warrant_status, fine_amount, court_cost FROM good_data_fixed WHERE date_of_birth = %s AND last_name LIKE %s AND drivers_license_number LIKE %s status <> \'CLOSED\' AND status <> \'DISMISS WITHOUT COSTS\'', (u[
-                'birthday'], u['last_name'], '%' + u))
+                'birthday'], u['last_name'], '%' + answer))
     result = cur.fetchall()
 
     cur.close()
